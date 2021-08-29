9c6c54d0d84fa9f541463055ce4f2de38034f87f
Ana Lucia <89168610+analucia-odp@users.noreply.github.com>
2021-08-29 16:05:04 +0000

    exe09

--- exe09/main.c
+++ exe09/main.c
@@ -5,23 +5,50 @@ int main() {
 
   struct Aluno
   {
-    int nota;
+    float nota;
     char nome[100];
   };
 
-  struct Aluno aluno[100];
+  struct Aluno aluno[100]; //declarando um vetor do tipo struct Aluno
 
-   int n;
-   float media=0;
+   int n, c1=0, c2=0, c3=0;
+   float media=0, m1=0, m2=0, m3=0;
    scanf("%d", &n);
 
    for(int cont=0; cont<n; cont++)
    {
-     scanf ("%s %d", aluno[cont].nome, &aluno[cont].nota);
+     scanf ("%s %f", aluno[cont].nome, &aluno[cont].nota);
+
+     if (aluno[cont].nota>m1)
+     {
+       m3 = m2;
+       m2 = m1;
+       m1 = aluno[cont].nota;
+       
+       c3 = c2;
+       c2 = c1;
+       c1 = cont;
+     }
+     else if (aluno[cont].nota>m2)
+     {
+       m3 = m2;
+       m2 = aluno[cont].nota;
+       
+       c3 = c2;
+       c2 = cont;
+     }
+     else if(aluno[cont].nota>m3)
+     {
+       m3 = aluno[cont].nota;
+       c3 = cont;
+     }
 
      media = media+aluno[cont].nota;
     }
 
-    printf("NOTA MEDIA = %.1f", media/n);
+    printf("NOTA MEDIA = %.1f\n", media/n);
+    printf ("%s %.1f\n", aluno[c3].nome, m3);
+    printf ("%s %.1f\n", aluno[c2].nome, m2);
+    printf ("%s %.1f\n", aluno[c1].nome, m1);
    return 0;
 }
